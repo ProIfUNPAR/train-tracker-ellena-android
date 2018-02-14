f1142d37ebab3204474f5c48f0d890990fd1b5de
billyAdi <billz.wew@gmail.com>
2018-02-14 07:49:11 +0700

    Revert "Parse tabel track"
    This reverts commit 7ef23edbcf232e76de9521b8cf929273e8c757ef.

--- app/src/main/java/com/tracker/train/traintracker/DBKereta.java
+++ app/src/main/java/com/tracker/train/traintracker/DBKereta.java
@@ -21,57 +21,13 @@ public class DBKereta {
 
         String query="SELECT * FROM track";
         Cursor cursor = mDBHelper.getmDataBase().rawQuery(query, null);
+
         if (cursor.moveToFirst()) {
             do {
                 //belum beres, blm bikin kelas kereta
-                /*System.out.print(" "+cursor.getString(1)); //nama stasiun
-                System.out.println(" "+cursor.getString(2)); // jadwal dan track, stasiun msh dlm bentuk id*/
-                String trackRaw = cursor.getString(2);
-                String s = "";
-                String route = "Route : ";
-                String schedule = "Jadwal: ";
-                boolean flag = true;
-                System.out.println("Raw : " + trackRaw);
-                System.out.print("Route : ");
-                for(int i = 0; i < trackRaw.length(); i++){
-                    if(flag) {
-                        if(trackRaw.charAt(i) != ';'){
-                            s += trackRaw.charAt(i);
-                        }
-                        else{
-                            //System.out.print("The ID is: " + s + ".");
-                            int id = Integer.parseInt(s);
-                            String stasiunQuery = "SELECT * FROM stasiun WHERE id = " + id;
-                            Cursor stasiunCur = mDBHelper.getmDataBase().rawQuery(stasiunQuery, null);
-                            if(stasiunCur.moveToFirst()) {
-                                route += stasiunCur.getString(1);
-                                flag = false;
-                                stasiunCur.close();
-                            }
-                            else{
-                                System.out.println("wtf error " + id);
-                            }
-                        }
-                    }
-                    else{
-                        if(trackRaw.charAt(i) == ','){
-                            flag = true;
-                            s = "";
-                            schedule += ",";
-                            route += " -> ";
-                        }
-                        else{
-                            if(trackRaw.charAt(i) != ';'){
-                                schedule += trackRaw.charAt(i);
-                            }
-                            else{
-                                schedule += "_";
-                            }
-                        }
-                    }
-                }
-                System.out.println(route);
-                System.out.println(schedule);
+                System.out.print(" "+cursor.getString(1)); //nama stasiun
+                System.out.println(" "+cursor.getString(2)); // jadwal dan track, stasiun msh dlm bentuk id
+
             } while (cursor.moveToNext());
         }
 
