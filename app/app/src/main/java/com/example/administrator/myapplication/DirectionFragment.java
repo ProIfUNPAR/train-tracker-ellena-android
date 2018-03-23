c163e576f25f58f0e311dcb2007aca20d5e2d2de
billyAdi <billz.wew@gmail.com>
2018-03-23 23:07:26 +0700

    penambahan manifest

--- app/app/src/main/java/com/example/administrator/myapplication/DirectionFragment.java
+++ app/app/src/main/java/com/example/administrator/myapplication/DirectionFragment.java
@@ -5,15 +5,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Switch;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
 
 import com.example.administrator.myapplication.Database.DBKereta;
 import com.example.administrator.myapplication.Database.DBStasiun;
 import com.example.administrator.myapplication.Database.DatabaseHelper;
 import com.example.administrator.myapplication.Database.Kereta;
 import com.example.administrator.myapplication.Database.Stasiun;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import java.util.ArrayList;
 
@@ -21,10 +36,12 @@ import java.util.ArrayList;
  * Created by user on 3/23/2018.
  */
 
-public class DirectionFragment extends Fragment implements View.OnClickListener{
+public class DirectionFragment extends Fragment implements View.OnClickListener,OnMapReadyCallback{
     protected Spinner keretaSpinner, asalSpinner, tujuanSpinner;
     protected Button searchBtn;
     protected Switch alarmSwitch;
+
+    protected Kereta selectedKereta;
 
     protected ArrayList<Stasiun> asalList;
     protected ArrayList<Stasiun> tujuanList;
@@ -68,14 +85,89 @@ public class DirectionFragment extends Fragment implements View.OnClickListener{
         this.dbStasiun=new DBStasiun(this.mDBHelper);
         this.stasiun=this.dbStasiun.getListStasiun();
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+        ArrayList<String> namaKereta = new ArrayList<String>();
+
+
+        for (int i = 0; i < kereta.size(); i++) {
+            namaKereta.add(kereta.get(i).getNama());
+        }
+
+        ArrayAdapter<Kereta> keretaArrayAdapter = new ArrayAdapter<Kereta>(getContext(), android.R.layout.simple_spinner_item, kereta);
+        keretaSpinner.setAdapter(keretaArrayAdapter);
+
+        keretaSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String nama = adapterView.getItemAtPosition(i).toString();
+                deleteList(asalList);
+                for (int x = 0; x < kereta.size(); x++) {
+                    if (kereta.get(i).getNama().equals(nama)) {
+                        selectedKereta = kereta.get(i);
+                        trackList = selectedKereta.getTrack();
+                        break;
+                    }
+                }
+                for (int x = 0; x < trackList.size() - 1; x++) {
+                    asalList.add(dbStasiun.getStasiunByName(trackList.get(x)));
+                }
+                ArrayAdapter<Stasiun> asalArrayAdapter = new ArrayAdapter<Stasiun>(getContext(), android.R.layout.simple_spinner_item, asalList);
+                asalSpinner.setAdapter(asalArrayAdapter);
+                asalSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        deleteList(tujuanList);
+
+                        for (int x = 0; x < trackList.size(); x++) {
+                            tujuanList.add(dbStasiun.getStasiunByName(trackList.get(x)));
+
+                        }
+                        int x = 0;
+                        while (x <= asalSpinner.getSelectedItemPosition()) {
+                            tujuanList.remove(0);
+                            x++;
+                        }
+
+                        ArrayAdapter<Stasiun> tujuanArrayAdapter = new ArrayAdapter<Stasiun>(getContext(), android.R.layout.simple_spinner_item, tujuanList);
+                        tujuanSpinner.setAdapter(tujuanArrayAdapter);
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                });
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
        return view;
     }
 
+    private void deleteList(ArrayList<Stasiun> list){
+        while(!list.isEmpty()){
+            list.remove(0);
+        }
+    }
 
     @Override
     public void onClick(View view) {
         if(view.getId()==this.searchBtn.getId()){
 
         }
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
