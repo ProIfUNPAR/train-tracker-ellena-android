3e49a48369af8229903507d43f9abb9866ac1ff2
adamadamadamadamadam <adamnurmishwari@gmail.com>
2018-04-05 08:34:18 +0700

    update schedule fragment

--- app/src/main/java/com/example/user/myapplication/Fragment/ScheduleFragment.java
+++ app/src/main/java/com/example/user/myapplication/Fragment/ScheduleFragment.java
@@ -1,5 +1,6 @@
 package com.example.user.myapplication.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -19,6 +21,7 @@ import com.example.user.myapplication.Database.DBStasiun;
 import com.example.user.myapplication.Database.DatabaseHelper;
 import com.example.user.myapplication.Database.Kereta;
 import com.example.user.myapplication.Database.Stasiun;
+import com.example.user.myapplication.ScheduleListAdapter;
 import com.example.user.myapplication.R;
 
 import java.util.ArrayList;
@@ -31,17 +34,20 @@ public class ScheduleFragment extends Fragment implements Runnable, View.OnClick
     private EditText etSearch;
     private ImageView ivButton;
     private ListView listResult;
+    private ListView listJadwal;
     protected DatabaseHelper mDBHelper;
     protected DBKereta dbKereta;
     protected DBStasiun dbStasiun;
     protected ArrayAdapter<Kereta> keretaAdapter;
+    protected ScheduleListAdapter jadwalAdapter;
 
     protected ArrayList<Stasiun> asalList;
     protected ArrayList<Stasiun> tujuanList;
     protected ArrayList<Kereta> kereta;
     protected ArrayList<Stasiun> stasiun;
     protected ArrayList<String> trackList;
-    ArrayList<Kereta> temp;
+    ArrayList<Kereta> tempKereta;
+    ArrayList<String> tempSchedule;
 
     public ScheduleFragment(){
 
@@ -54,10 +60,12 @@ public class ScheduleFragment extends Fragment implements Runnable, View.OnClick
         this.etSearch = view.findViewById(R.id.et_train_search);
         this.ivButton = view.findViewById(R.id.iv_get_schedule_btn);
         this.listResult = view.findViewById(R.id.listview_route);
-        this.mDBHelper=new DatabaseHelper(this.getContext());
-        this.dbKereta=new DBKereta(this.mDBHelper);
+        this.mDBHelper= new DatabaseHelper(this.getContext());
+        this.dbKereta= new DBKereta(this.mDBHelper);
         this.kereta = this.dbKereta.getListTrains();
-        this.temp = new ArrayList<>();
+        this.listJadwal = view.findViewById(R.id.listview_schedule);
+        this.tempKereta = new ArrayList<>();
+        this.tempSchedule = new ArrayList<>();
         etSearch.addTextChangedListener(new CustomTextWatcher());
         listResult.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -83,9 +91,9 @@ public class ScheduleFragment extends Fragment implements Runnable, View.OnClick
 
     public void searchItem(String textToSearch){
         initList();
-        for(int i = 0; i < temp.size(); i++){
-            if(!temp.get(i).getNama().toLowerCase().contains(textToSearch)){
-                temp.remove(i);
+        for(int i = 0; i < tempKereta.size(); i++){
+            if(!tempKereta.get(i).getNama().toLowerCase().contains(textToSearch)){
+                tempKereta.remove(i);
                 i--;
             }
         }
@@ -93,16 +101,48 @@ public class ScheduleFragment extends Fragment implements Runnable, View.OnClick
     }
 
     public void initList(){
-        this.temp.clear();
-        this.temp.addAll(kereta);
-        this.keretaAdapter = new ArrayAdapter<>(getActivity(), R.layout.string_list_kereta, temp);
+        this.tempKereta.clear();
+        this.tempKereta.addAll(kereta);
+        this.keretaAdapter = new ArrayAdapter<>(getActivity(), R.layout.string_list_kereta, tempKereta);
         this.listResult.setAdapter(keretaAdapter);
+    }
+
+    public void filterSchedule(Kereta x){
+        this.scheduleListInit(x);
+        Log.d("clicktrigger", "blah");
+        for(int i = 0; i < x.getTrack().size(); i++){
+            for(int j = 0; j < tempSchedule.size(); j++){
+                if(tempSchedule.get(j).equals(x.getTrack().get(i))){
+                    tempSchedule.remove(j);
+                    j--;
+                }
+            }
+        }
+        jadwalAdapter.notifyDataSetChanged();
+    }
+
+    public void scheduleListInit(Kereta x){
+        this.tempSchedule.clear();
+        Log.d("debugStation", x.getNama());
+        this.tempSchedule.addAll(x.getTrack());
+        this.jadwalAdapter = new ScheduleListAdapter(this, x);
+        this.listJadwal.setAdapter(jadwalAdapter);
     }
 
     @Override
     public void onClick(View view) {
         if(view.getId() == ivButton.getId()){
-            Log.d("clicktrigger", "clicked");
+            if(!etSearch.getText().toString().equals("")){
+                for(int i = 0; i < kereta.size(); i++){
+                    if(kereta.get(i).toString().equals(etSearch.getText().toString())){
+                        this.filterSchedule(kereta.get(i));
+                        listJadwal.bringToFront();
+                        break;
+                    }
+                }
+            }
+            InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
     }
 
@@ -115,6 +155,7 @@ public class ScheduleFragment extends Fragment implements Runnable, View.OnClick
 
         @Override
         public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            listResult.bringToFront();
             if(charSequence.toString().equals("")){
                 //reset listview
                 initList();
---
+++ app/src/main/java/com/example/user/myapplication/ScheduleListAdapter.java
@@ -0,0 +1,87 @@
+package com.example.user.myapplication;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.user.myapplication.Database.Kereta;
+import com.example.user.myapplication.Fragment.ScheduleFragment;
+import com.example.user.myapplication.R;
+
+import java.util.List;
+
+/**
+ * Created by Asus on 05/04/2018.
+ */
+
+public class ScheduleListAdapter extends BaseAdapter {
+
+    private ScheduleFragment scheduleFragment;
+    private List<String> stasiunList, arrivalList, departureList;
+
+    public ScheduleListAdapter(ScheduleFragment scheduleFragment, Kereta kereta){
+        this.stasiunList = kereta.getTrack();
+        this.arrivalList = kereta.getWaktuTiba();
+        this.departureList = kereta.getWaktuBerangkat();
+        this.scheduleFragment = scheduleFragment;
+        //Log.d("debugStation", String.valueOf(stasiunList.size()));
+        //Log.d("debugStation", String.valueOf(arrivalList.size()));
+        //Log.d("debugStation", String.valueOf(departureList.size()));
+    }
+
+    @Override
+    public int getCount() {
+        return stasiunList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        String[] item = new String[3];
+        item[0] = stasiunList.get(i);
+        item[1] = arrivalList.get(i);
+        item[2] = departureList.get(i);
+        return item;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        final ViewHolder viewHolder;
+        if(view == null){
+            view = LayoutInflater.from(this.scheduleFragment.getActivity()).inflate(R.layout.string_list_jadwal, viewGroup, false);
+            viewHolder = new ViewHolder(view);
+            view.setTag(viewHolder);
+        }
+        else{
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        final String[] stringArr = (String[]) this.getItem(i);
+        viewHolder.updateView(stringArr[0], stringArr[1], stringArr[2]);
+        return view;
+    }
+
+    private class ViewHolder{
+        protected TextView tvStation;
+        protected TextView tvArrival;
+        protected TextView tvDeparture;
+
+        public ViewHolder(View view){
+            this.tvStation = view.findViewById(R.id.tv_schedule_station);
+            this.tvArrival = view.findViewById(R.id.tv_schedule_arrival);
+            this.tvDeparture = view.findViewById(R.id.tv_schedule_departure);
+        }
+
+        public void updateView(String station, String arrival, String departure){
+            this.tvStation.setText(station);
+            this.tvArrival.setText(arrival);
+            this.tvDeparture.setText(departure);
+        }
+    }
+}
