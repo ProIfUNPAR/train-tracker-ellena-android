ded4b6d6da9db618253b13e941421be19ca446c1
adamadamadamadamadam <adamnurmishwari@gmail.com>
2018-04-11 11:12:10 +0700

    update warning gps

--- app/src/main/java/com/example/user/myapplication/Activity.java
+++ app/src/main/java/com/example/user/myapplication/Activity.java
@@ -1,8 +1,13 @@
 package com.example.user.myapplication;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -24,36 +29,57 @@ public class Activity extends AppCompatActivity {
         iv = (ImageView) findViewById(R.id.iv);
         Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
         iv.startAnimation(myanim);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            Log.d("debugpermis", "AB");
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                    0);
+        final LocationManager locManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if(!locManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            noGPSAlert();
         }
-        else{
-            final Intent i = new Intent(this, MainActivity.class);
-            Thread timer = new Thread() {
-                public void run() {
-                    try {
-                        sleep(4000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } finally {
-                        startActivity(i);
-                        finish();
+        else {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                Log.d("debugpermis", "AB");
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        0);
+            } else {
+                final Intent i = new Intent(this, MainActivity.class);
+                Thread timer = new Thread() {
+                    public void run() {
+                        try {
+                            sleep(4000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } finally {
+                            startActivity(i);
+                            finish();
+                        }
                     }
-                }
-            };
-            timer.start();
+                };
+                timer.start();
+            }
         }
+    }
+
+    private void noGPSAlert() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("GPS is disabled. Please turn on.").setCancelable(false)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                        Intent intent = getIntent();
+                        finish();
+                        startActivity(intent);
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
     }
 
     @Override
