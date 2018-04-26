bfa7130523c0ec280bd738760ed4d8e425c35ea6
EllenaAngelica <ellenaangelica98@gmail.com>
2018-04-26 08:05:09 +0700

    Ganti tulisan alarm

--- app/src/main/java/com/example/user/myapplication/ListViewSecondActivity.java
+++ app/src/main/java/com/example/user/myapplication/ListViewSecondActivity.java
@@ -29,7 +29,7 @@ public class ListViewSecondActivity extends AppCompatActivity {
         Bundle bundle = getIntent().getExtras();
         if(bundle != null){
             mToolbar.setTitle(bundle.getString("OptionAlarm"));
-            if(mToolbar.getTitle().toString().equalsIgnoreCase("Choose Mode")){
+            if(mToolbar.getTitle().toString().equalsIgnoreCase("Pilih Mode")){
                 lv = findViewById(R.id.modeAlarmListView);
                 lv.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,
                         getResources().getStringArray(R.array.OptionAlarmMode)));
@@ -37,20 +37,20 @@ public class ListViewSecondActivity extends AppCompatActivity {
                     @Override
                     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                         String s = lv.getItemAtPosition(i).toString();
-                        if(s.equalsIgnoreCase("Just Vibrate")){
+                        if(s.equalsIgnoreCase("Mode getar saja")){
                             alarm_mode = 0;
                         }
-                        else if(s.equalsIgnoreCase("Just Alarm")){
+                        else if(s.equalsIgnoreCase("Mode suara saja")){
                             alarm_mode = 1;
                         }
-                        else if(s.equalsIgnoreCase("Alarm and Vibrate")){
+                        else if(s.equalsIgnoreCase("Mode suara dan getar")){
                             alarm_mode = 2;
                         }
-                        Toast.makeText(ListViewSecondActivity.this, "Mode has changed to " + s, Toast.LENGTH_LONG).show();
+                        Toast.makeText(ListViewSecondActivity.this, "Mode telah diubah ke " + s, Toast.LENGTH_LONG).show();
                     }
                 });
             }
-            if(mToolbar.getTitle().toString().equalsIgnoreCase("Choose Ringtone")){
+            if(mToolbar.getTitle().toString().equalsIgnoreCase("Pilih Ringtone")){
                 lv = findViewById(R.id.modeAlarmListView);
                 lv.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,
                         getResources().getStringArray(R.array.OptionRingtoneMode)));
@@ -67,7 +67,7 @@ public class ListViewSecondActivity extends AppCompatActivity {
                         else if(s.equalsIgnoreCase("Lucky2")){
                             ringtone_mode = 2;
                         }
-                        Toast.makeText(ListViewSecondActivity.this, "Ringtone set to " + s, Toast.LENGTH_LONG).show();
+                        Toast.makeText(ListViewSecondActivity.this, "Ringtone telah diatur menjadi " + s, Toast.LENGTH_LONG).show();
                     }
                 });
             }
